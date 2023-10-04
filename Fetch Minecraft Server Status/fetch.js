11e543114c342bd40afb136f37fd131924f6a536
Leaf-7 <140615908+Leaf-7@users.noreply.github.com>
2023-10-04 18:07:04 +0100

    Create fetch.js

---
+++ Fetch Minecraft Server Status/fetch.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const { mcservstatus } = require("../../logic/index.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('fetch')
+        .setDescription('Fetches server info on a Minecraft server!')
+        .addStringOption(option =>
+            option.setName('ip')
+                .setDescription('The Ip address of the Minecraft Server.')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const ip = interaction.options.getString('ip')
+        let info = await mcservstatus(ip)
+        if (info != "The server is currently offline!"){
+            const mcServStat = new EmbedBuilder()
+                .setColor(0x11CF16)
+                .setTitle(ip)
+                .setDescription(info)
+            await interaction.reply({ embeds: [mcServStat] });
+        }
+        else {
+            const mcServStat = new EmbedBuilder()
+                .setColor(0xFF2F2F)
+                .setTitle(ip)
+                .setDescription(info)
+            await interaction.reply({ embeds: [mcServStat] });
+        }
+    },
+};
